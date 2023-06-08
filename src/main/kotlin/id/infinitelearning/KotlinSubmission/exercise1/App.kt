934e9b1de5827411a34ce323dac255e536cd6a9b
typeNull <rafydjauhari15@gmail.com>
2023-06-08 23:01:24 +0700

    first commit

---
+++ src/main/kotlin/id/infinitelearning/KotlinSubmission/exercise1/App.kt
@@ -0,0 +1,84 @@
+package id.infinitelearning.KotlinSubmission.exercise1
+
+/**
+Latihan 1
+Lengkapi fungsi myProfile di bawah ini dengan membuat variabel dengan ketentuan dibawah ini:
+- Variable bertipe data string yang menyimpan nilai nama depan Anda.
+- Variable bertipe data string yang menyimpan nilai nama belakang Anda.
+- Variable bertipe data number yang menyimpan nilai umur Anda.
+- Variable bertipe data boolean yang menyimpan nilai status Anda (single atau tidak)
+- Dan cetak setiap variabel ke layar saat fungsi myProfile() di panggil
+ */
+fun myProfile() {
+    val firstName: String = "Rafy"
+    val lastName: String = "Djauhari"
+    val age: Int = 10
+    val isSingle: Boolean = false
+
+    println("Nama Depan: $firstName")
+    println("Nama Belakang: $lastName")
+    println("Umur: $age")
+    println("Status: ${if (isSingle) "Single" else "Tidak Single"}")
+}
+
+
+/**
+ *  Latihan 2
+ *  Lengkapi fungsi di bawah ini agar dapat mencetak nilai dari parameter-parameter yang ada dengan fungsi println
+ */
+fun groupDetail(groupId: Int, groupMember: List<Any>, session: String): Any {
+    println("Group ID: $groupId")
+    println("Group Member: $groupMember")
+    println("Session: $session")
+
+    return arrayOf(groupId, groupMember, session)
+}
+
+/**
+ * Latihan 3
+ * Buat variable yang berisi daftar anggota group kamu,
+ * Kemudian akses item yang berisi nama Anda dari variable tersebut, lalu jadikan nilai kembalian untuk fungsi myGroup ini
+ *
+ */
+fun myGroup(): String {
+    val groupMembers = listOf("Ronaldo", "Neymar", "Messi", "Mbappe", "Rafy", "Halland")
+    val myName = groupMembers[4]
+
+    return myName
+}
+
+/**
+ * Latihan 4
+ * Ubah nilai kembalian untuk fungsi totalMember ini dengan rumus:
+ *
+ *      total mentor + jumlah anggota group
+ *
+ */
+fun totalMember(): Int {
+    val mentor = arrayOf("Dian", "Shasa", "Hasan", "Reza")
+    val countOfGroup = 5
+
+    val totalMentor = mentor.size
+    val totalMember = totalMentor + countOfGroup
+
+    return totalMember
+}
+
+fun main() {
+
+    myProfile()
+
+    val myGroup = myGroup()
+    println("My Group is: $myGroup")
+
+    val totalMember = totalMember()
+    println("Total Member group + Mentor: $totalMember")
+
+    /**
+     *  Latihan 5
+     *  Ubah nilai argumen-argumen dari fungsi groupDetail di bawah ini sesuai dengan data group kamu
+     *
+     */
+    groupDetail(4, listOf("Ronaldo", "Neymar", "Messi", "Mbappe", "Rafy", "Halland"), "Morning")
+
+}
